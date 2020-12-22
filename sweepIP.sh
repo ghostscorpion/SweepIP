39a0c7f7ee9398ebcade66ffa54dd194eb4dec65
ghostscorpion <ghostscorpion@gmail.com>
2020-12-22 15:03:35 -0600

    Update sweepIP.sh
    update 
     removed the seq 
    and {1..254}; to replace the old seq that no longer works

--- sweepIP.sh
+++ sweepIP.sh
@@ -4,16 +4,18 @@
 # By Ghostscorpion
 
 echo "What IP range do you wish to sweep for live systems on"
+echo "Enter the first three octets of the IP range."
 echo "Syntax: 192.168.1"
 read ip
 
+if [ "$1" == "" ]
+then
+  echo "Need first three octet of the IP address"
+  echo "Syntax : ./ipsweep.sh 192.168.1"
 
-if [ "$ip" == "" ] 
-        then 
-                echo "You forgot an IP Address!"
-                echo "Syntax: ./sweepIP.sh 192.168.1"
-        else
-                for n in `seq 1 254`; do
-                ping -c 1  $ip.$n | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> "$ip"_list.txt &
-        done
+else
+ for ip in {1..254};
+  do
+   ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+ done
 fi
